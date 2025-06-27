c87336337da918784aeba2438269b2f992505b6c
Dương Nguyễn Thị Thùy <thuyduong18392@gmail.com>
2025-06-27 12:53:12 +0700

    Add files via upload

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,5 +1,24 @@
 <?php
 function university_post_types() {
+  // Campus Post Type
+  register_post_type('campus', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'excerpt'),
+    'rewrite' => array('slug' => 'campuses'),
+    'has_archive' => true,
+    'public' => true,
+    'show_in_rest' => true,
+    'labels' => array(
+      'name' => 'Campuses',
+      'add_new_item' => 'Add New Campus',
+      'edit_item' => 'Edit Campus',
+      'all_items' => 'All Campuses',
+      'singular_name' => 'Campus',
+    ),
+    'menu_icon' => 'dashicons-location-alt'
+  ));
+
+  // Event Post Type
   register_post_type('event', array(
     'show_in_rest' => true,
     'supports' => array('title', 'editor', 'excerpt'),
@@ -15,6 +34,41 @@ function university_post_types() {
       'singular_name' => 'Event',
     ),
     'menu_icon' => 'dashicons-calendar'
+  ));
+
+  // Program Post Type
+  register_post_type('program', array(
+    'show_in_rest' => true,
+    'supports' => array('title'),
+    'rewrite' => array('slug' => 'program'),
+    'has_archive' => true,
+    'public' => true,
+    'show_in_rest' => true,
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new_item' => 'Add New Program',
+      'edit_item' => 'Edit Program',
+      'all_items' => 'All Programs',
+      'singular_name' => 'Program',
+    ),
+    'menu_icon' => 'dashicons-awards'
+  ));
+
+  // Professor Post Type
+  register_post_type('professor', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'public' => true,
+    'show_in_rest' => true,
+    'labels' => array(
+      'name' => 'Professors',
+      'add_new_item' => 'Add New Professor',
+      'edit_item' => 'Edit Professor',
+      'all_items' => 'All Professors',
+      'singular_name' => 'Professor',
+      'show_in_rest' => true,
+    ),
+    'menu_icon' => 'dashicons-welcome-learn-more'
   ));
 }
 
